df12cdce1f43281fc12bd1cedc3dab0b9b98ee89
moomoocow5432 <ianmcqlee@gmail.com>
2025-05-17 16:20:45 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import random
+
+"""Creates a 52 card deck, returns a list of strings"""
+def create_deck():
+    deck = []
+    for l in range(4):
+        for j in range(1, 14):
+            if j == 1:
+                deck.append("Ace")
+            elif j < 11:
+                deck.append(str(j))
+            elif j == 11:
+                deck.append("Jack")
+            elif j == 12:
+                deck.append("Queen")
+            elif j == 13:
+                deck.append("King")
+    return deck
+    
+"""Input a deck of cards, output is a shuffled deck"""
+def shuffle(deck):
+    new_deck = []
+    for i in range(51):
+        num = 51 - i
+        card = random.randint(0, num)
+        new_deck.append(deck[card])
+        deck.pop(card)
+    return(new_deck)
+
+deck_1 = create_deck()
+print(deck_1)
+print(shuffle(deck_1))
